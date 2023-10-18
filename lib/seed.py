95133970df584ea01c6d3e631e434a3644142e01
Cooper <55swimmer55@gmail.com>
2023-10-18 17:13:08 -0400

    adds dog food objects for database

--- lib/seed.py
+++ lib/seed.py
@@ -50,6 +50,31 @@ def seed_database():
     cat_chocolate = AnimalFood.create(cat.id, chocolate.id, "false")
     cat_sweet_potatoes = AnimalFood.create(cat.id, sweet_potatoes.id, "false")
 
+    # dog
+    dog_dog_food = AnimalFood.create(dog.id, dog_food.id, "true")
+    dog_avocado = AnimalFood.create(dog.id, avocado.id, "false")
+    dog_grapes = AnimalFood.create(dog.id, grapes.id, "false")
+    dog_garlic = AnimalFood.create(dog.id, garlic.id, "false")
+    dog_xylitol = AnimalFood.create(dog.id, xylitol.id, "false")
+    dog_nuts = AnimalFood.create(dog.id, nuts.id, "false")
+    dog_butter = AnimalFood.create(dog.id, butter.id, "false")
+    dog_nutmeg = AnimalFood.create(dog.id, nutmeg.id, "false")
+    dog_onion = AnimalFood.create(dog.id, onion.id, "false")
+    dog_liver = AnimalFood.create(dog.id, liver.id, "true")
+    dog_citrus = AnimalFood.create(dog.id, citrus.id, "true")
+    dog_corn = AnimalFood.create(dog.id, corn.id, "true")
+    dog_peanuts = AnimalFood.create(dog.id, peanuts.id, "true")
+    dog_apple_seeds = AnimalFood.create(dog.id, apple_seeds.id, "true")
+    dog_bread = AnimalFood.create(dog.id, bread.id, "true")
+    dog_banana = AnimalFood.create(dog.id, banana.id, "true")
+    dog_watermelon =AnimalFood.create(dog.id, watermelon.id, "true")
+    dog_carrot = AnimalFood.create(dog.id, carrot.id, "true")
+    dog_salt = AnimalFood.create(dog.id, salt.id, "true")
+    dog_dairy = AnimalFood.create(dog.id, dairy.id, "true")
+    dog_non_toxic_mushrooms = AnimalFood.create(dog.id, non_toxic_mushrooms.id, "true")
+    dog_pumpkin = AnimalFood.create(dog.id, pumpkin.id, "true")
+    dog_broccoli = AnimalFood.create(dog.id, broccoli.id, "true")
+
     # dictionary = AnimalFood.all
     # print(dictionary)
     # id = 1
