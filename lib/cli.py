a8d3212270be2b66e21f509a19d9749340baeef2
Christopher Orth <christopherorth430@yahoo.com>
2023-10-19 16:28:09 -0400

    Final product before cooper

--- lib/cli.py
+++ lib/cli.py
@@ -1,20 +1,24 @@
 # lib/cli.py
 from seed import Animal, AnimalFood, Food
 
+
+
 from helpers import (
     exit_program,
     delete_animal,
     create_animal,
     list_foods,
-    list_animals,
-    update_animal_name,
-    unsafe_foods_for_animal
+    list_animals, 
+    unsafe_foods_for_animal,
+    update_animal_name
 )
 
 def main():
     while True:
+        # from models.AnimalFood import AnimalFood
+        # print(AnimalFood.all)
         menu()
-        choice = input("> ")
+        choice = input("\n> ")
         if choice == "0":
             exit_program()
         elif choice == "1":
@@ -22,25 +26,24 @@ def main():
         elif choice == "2":
             delete_animal()
         elif choice == "3":
-            list_foods()
-        elif choice == "4":
-            list_animals()
-        elif choice == "5":
             update_animal_name()
+        elif choice == "4":
+            list_foods()
+        elif choice == "5":
+            list_animals()
         elif choice == "6":
             unsafe_foods_for_animal()
         else:
-            print("Invalid choice")
+            print("----------------\n <!> Invalid choice <!> \n----------------")
 
 def menu():
-    print("--- Main Menu ---------------------------------------------------------------------")
-    print("Please select an option:")
-    print("0. Exit the program")
+    print("\n\n----- Please select an option -----\n")
+    print("0. Exit le program")
     print("1. Add a new animal")
-    print("2. Delete animal")
-    print("3. List all currently supported foods from database")
-    print("4. List all currently supported animals from database")
-    print("5. Update an animal's name")
+    print("2. Delete an animal")
+    print("3. Update an animal's name")
+    print("4. List all currently supported foods from database")
+    print("5. List all currently supported animals from database")
     print("6. List all unsafe foods for an animal")
 
 if __name__ == "__main__":
--- lib/helpers.py
+++ lib/helpers.py
@@ -3,69 +3,259 @@ from models.Animal import Animal
 from models.AnimalFood import AnimalFood
 from models.Food import Food
 
-# Delete row of Animal table by name attribute
-def delete_animal():
-    name = input("Enter the name of the animal to delete: ")
-    animal = Animal.find_by_name(name)
-    id = animal.id
 
-    ids = animal.animal_foods_ids()
-    print(AnimalFood.all)
-    for id in ids:
-        del AnimalFood.all[id]
-        animal.delete_animal_foods(id)
 
-    animal.delete(id)
-    print(f"{name} deleted from the database")
-    print(AnimalFood.all)
 
-# Create an animal instance and store it in 'animals' table when given a name
-def create_animal():
-    """ """
-    name = input("Enter animal name: ")
-    # Input validation: Ensure the name is not empty.
-    if not name and not isinstance(name, str):
-        print("Animal name cannot be empty and must be a string.")
-        return
-    animal = Animal.create(name.lower().strip())
-    print(f"Animal '{animal.name}' created.")
+
+
+
+# --------------------------------------------------------------
+# Below is the helper to EXIT THE PROGRAM ENTIRELY
+# --------------------------------------------------------------
+
 
 def exit_program():
-    print("Goodbye!")
+    print("You have now exited the program")
+    print("---------- Goodbye! ----------\n\n")
     exit()
 
-# Get all names from 'foods' table
-def list_foods():
-    foods = Food.get_all_food()
-    for food in foods:
-        print(food.name)
+
+
+
+
+
+
+
+
+
+# --------------------------------------------------------------
+# Below is the helper to CREAT A NEW ANIMAL
+# --------------------------------------------------------------
+
+
+
+
+
+
+def create_animal():
+    name = input("Enter animal name: ").strip().lower()
+    # Input validation: make damn sure the name is not empty.
+    if not name:
+        return
     
-# Get all names from 'animals' table
+    if name == "cooper":
+        print(f"------------------------------------------------------------\n <!> CATUION <!> \n------------------------------------------------------------ \n We do not allow 'Coopers' \n------------------------------------------------------------ \n'Cooper' cannot be added to the database. Creation canceled.\n------------------------------------------------------------")
+        return
+    # Fetch the list of existing animals
+    existing_animals = [animal.name.lower() for animal in Animal.get_all_animal()]
+    if name in existing_animals:
+        print(f"------------------------------------------------------------\nAnimal '{name.capitalize()}' already exists in the database. Creation canceled.\n------------------------------------------------------------")
+        return
+
+    # confirmation
+    confirmation = input(f"---------------------------------------------------------------------\nAre you sure you want to add '{name.capitalize()}' to the database? (yes/no):\n---------------------------------------------------------------------\nResponse: ").strip().lower()
+    if confirmation == "yes":
+        animal = Animal.create(name)
+        print(f"---------------------------------------------------\nAnimal '{animal.name.capitalize()}' created and added to the Database.\n---------------------------------------------------")
+    else:
+        print(f"-----------------------------\n <!> Add '{name.capitalize()}' was canceled. <!> \n-----------------------------")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# --------------------------------------------------------------
+# Below is the helper to DELETE AN EXISTING ANIMAL
+# --------------------------------------------------------------
+
+
+
+
+
+def delete_animal():
+    name = input("-----------------------------------------\nEnter the name of the animal to delete: ").lower()
+    animal = Animal.find_by_name(name)
+    if not animal:
+        print(f" <!> Animal '{name}' not found <!> ")
+        return
+
+    if name.lower() == "dog":
+        # Ask for double confirmation
+        confirmation = input(f"----------------------------------------------------------------------------------------------------\nAre you sure you want to delete '{name}' and be known as the 'Dog Deleter'??? Type 'yes' to confirm: \n----------------------------------------------------------------------------------------------------\nResponse: ").strip().lower()
+        if confirmation == "yes":
+            second_confirmation = input(f"----------------------------------------------------------------------------------------------------\nWooooooooow, I see what kind of person you are... so you really want to delete '{name}'? Type 'yes' to confirm your savagery: \n----------------------------------------------------------------------------------------------------\nResponse: ").strip().lower()
+            if second_confirmation == "yes":
+                
+                # Proceed with dog deletion
+                id = animal.id
+                ids = animal.animal_foods_ids()
+
+                for id in ids:
+                    del AnimalFood.all[id]
+                    animal.delete_animal_foods(id)
+
+                animal.delete(id)
+                print(f"----------------------------------------\nThe sweet sweet'{name}' was deleted from the database, I hope you're happy you sick twisted monster\n----------------------------------------")
+                # print(AnimalFood.all)
+            else:
+                print(f"-----------------------------------------------------------------------\nI freakin thought so you sick son of a sideways salamander.... Deletion of the adorable '{name}' was canceled.\n-----------------------------------------------------------------------")
+        else:
+            print("-------------------------\n <!> Deletion canceled. <!> \n-------------------------")
+    else:
+        # Ask for regular confirmation
+        confirmation = input(f"----------------------------------------\nAre you sure you want to delete '{name}'? (yes/no): \n----------------------------------------\nResponse: ").strip().lower()
+
+        if confirmation == "yes":
+            id = animal.id
+            ids = animal.animal_foods_ids()
+
+            # Regular delete
+            for id in ids:
+                del AnimalFood.all[id]
+                animal.delete_animal_foods(id)
+            animal.delete(id)
+            print(f"------------------------------------\n <!> '{name}' deleted from the database <!> \n------------------------------------")
+            # print(AnimalFood.all)
+        elif confirmation == "no":
+            print(f"------------------------------------\n<!> Deletion of '{name}' canceled. <!>\n------------------------------------")
+        else:
+            print("--------------------------------------------\n<!> Invalid input. Please enter 'yes' or 'no' for confirmation. <!>\n--------------------------------------------")
+
+
+
+
+
+
+
+
+
+
+
+# --------------------------------------------------------------
+# Below is the helper to LIST ALL FOODS
+# --------------------------------------------------------------
+
+
+
+def list_foods():
+    print("--------------\nFood List:\n--------------")
+    foods = Food.get_all_food()
+
+    
+    food_name = [food.name for food in foods]
+    sorted_food_names = sorted(food_name)
+
+    for food in sorted_food_names:
+        print(f"- {food}")
+    print(" ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+# --------------------------------------------------------------
+# Below is the helper to LIST ALL ANIMALS
+# --------------------------------------------------------------
+
 def list_animals():
+    print("--------------\nSpecies List:\n--------------")
     animals = Animal.get_all_animal()
-    for animal in animals:
-        print(animal.name)
+
+    # Extracted animal names and sorted them within the list_animals function
+    animal_names = [animal.name for animal in animals]
+    sorted_animal_names = sorted(animal_names)
+
+    for name in sorted_animal_names:
+        print(f"- {name}")
+    print(" ")
+
+
+
+
+
+
+
+
+
+
+
+
+# --------------------------------------------------------------
+# Below is the helper to UPDATE EXISTING ANIMAL NAME
+# --------------------------------------------------------------
 
 def update_animal_name():
-    current_name = input("Type the animal whose name you wish to change: ")
+    current_name = input("----------------------------------------------------\nType the animal whose name you wish to change: ")
     animal = Animal.find_by_name(current_name)
     if not animal:
-        print(f"Animal '{current_name}' not found")
+        print(f"----------------------------\n <!> Animal '{current_name}' not found <!> \n----------------------------")
         return
-    new_name = input("Type the new name you wish to save: ")
-    animal.update(new_name)
-    print(f"The name of animal '{current_name} has been updated to '{new_name}")
+    while True:
+        new_name = input(f"-------------------------------------------\nWhat would you like to switch '{current_name}' to: ")
+        # Checks if new_name is an empty string
+        if not new_name.strip():  
+            print("----------------------------------------------------\n!!!>>>>  Please provide a non-empty name.  <<<<!!!")
+        else:
+            break
+    
+    while True:
+        confirmation = input(f"-----------------------------------------\nDo you want to update '{current_name}' to '{new_name}'? (yes/no)\n-----------------------------------------\nResponse: ").strip().lower()
+        if confirmation == "yes":
+            animal.update(new_name)
+            print(f"--------------------------------------------------\nThe name of animal '{current_name}' has been updated to '{new_name}'\n--------------------------------------------------")
+            break
+        elif confirmation == "no":
+            print(f"------------------------------------\n <!> Update '{current_name}' name canceled. <!> \n------------------------------------\n\n")
+            break
+        else:
+            print("------------------------------------\n!!!>>>>  Please Confirm (yes/no).  <<<<!!!")
+
+
+
+
+
+
+
+
+
+
+# ------------------------------------------------------------------
+# Below is the helper to LIST ALL UNSAFE FOODS FOR A SPECIFIC ANIMAL
+# ------------------------------------------------------------------
+
+
+
+
+
+
 
 def unsafe_foods_for_animal():
     #ask for a species
-    animal_name = input("Which animal would you like to check: ")
+    animal_name = input("\n\nWhich animal would you like to check: ")
     # print(animal_name)
 
     #get the id of the species from animal table
     animal = Animal.find_by_name(animal_name)
-    print(animal)
+    # print(animal)
     if not animal:
-        print(f"Animal '{current_name}' not found")
+        print(f"Animal '{animal_name}' not found")
         return
     
     #get all animal_foods ids for that animal
@@ -99,6 +289,18 @@ def unsafe_foods_for_animal():
     sorted_names = sorted(food_names)
 
     #print the names
-    print(f"Unsafe Foods for '{animal_name}:")
+    print(f"\n------------------------\nUnsafe Foods for '{animal_name}':\n------------------------")
     for name in sorted_names:
-        print(name)
+        print(f"- {name}")
+
+
+
+
+
+
+
+
+
+# --------------------------------------------------------------
+# Below is the helper to SHOW WEBMD RESULTS
+# --------------------------------------------------------------
